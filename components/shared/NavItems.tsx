c8c45eede1be9c9eecf0e284b23a6b9242ddf684
Hank Kapka <hankkapka@gmail.com>
2024-01-12 08:20:00 -0600

    updated header

---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,20 @@
+import { headerLinks } from '@/constants'
+import Link from 'next/link'
+
+import React from 'react'
+
+const NavItems = () => {
+    return (
+        <ul className="md:flex-between flex">
+            {headerLinks.map((link) => {
+                return (
+                    <li>
+                        <Link href={link.route}>{link.label}</Link>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+export default NavItems
